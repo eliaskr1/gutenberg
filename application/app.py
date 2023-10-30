94c3e17bffaac6593b24823a931901cf17c5996c
Elias Kroon <elias.kroon19@gmail.com>
2023-10-30 12:53:30 +0100

    flask struktur skapad

---
+++ application/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import urllib.request
+import ssl
+import pandas as pd
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/form")
+def form():
+    return render_template("form.html")
+
+@app.route("/api", methods=["POST"])
+def api():
+    return render_template("api.html")
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("index.html"), 404
