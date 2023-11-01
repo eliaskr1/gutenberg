edb57fa523859aa0460cd8efbb7048048d81b2cc
Benjamin Makela <benjamin.makela@yh.nackademin.se>
2023-11-01 13:38:11 +0100

    Fixade att den sparar cookie för senaste sökningen (text endast för nu). Jag uppdaterade även form.html för att man kan gå tillbaka dit (då autofylls senaste sökningen) med en knapp på /api och ändrade dropdown menu där lite snabbt. Kan gjort något mer små fix men kommer inte ihåg...

--- application/app.py
+++ application/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 import urllib.request
 import ssl
 import pandas as pd
@@ -9,6 +9,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+
     with open('static/languages.json', "r", encoding="utf-8") as f:
         langs = json.load(f)
     with open('static/topics.json', "r", encoding="utf-8") as f:
@@ -17,13 +18,25 @@ def index():
 
 @app.route("/form")
 def form():
-    return render_template("form.html")
+
+    with open("static/languages.json", "r", encoding="utf-8") as f:
+        langs = json.load(f)
+    with open("static/topics.json", "r", encoding="utf-8") as f:
+        topics = json.load(f)
+
+    previous_search = request.cookies.get("search_query", "")
+
+    return render_template("form.html", langs=langs, topics=topics, previous_search=previous_search)
 
 @app.route("/api", methods=["POST"])
 def api_post():
-    user_input = request.form.get("search", "").strip()  # Using strip to remove any leading/trailing spaces
+
+    user_input = request.form.get("search_query", "").strip()  # Using strip to remove any leading/trailing spaces
     topic = request.form.get("topic", "").strip()
     language = request.form.get("language", "").strip()
+
+    # Set or update the latest search
+    previous_search = request.cookies.get("search_query", "")
 
     # Create a list to store valid query parameters
     params = []
@@ -45,8 +58,17 @@ def api_post():
     api_url = f"{base_url}?{'&'.join(params)}"
 
     data = func.json_data_to_html_table(api_url)
-    return render_template("table.html", data=data)
+
+    # Set or update the search_query cookie
+    resp = make_response(render_template("table.html", previous_search=previous_search, saved_search=user_input, data=data))
+    resp.set_cookie("search_query", user_input)
+
+    return resp  # Returns the response with the cookie for the POST request
+
+
 
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("index.html"), 404
+
+
